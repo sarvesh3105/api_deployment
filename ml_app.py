ff92c9aaff6eca7bfe860574868117cd31b66e1f
sarvesh3105 <145481210+sarvesh3105@users.noreply.github.com>
2024-07-20 21:17:16 +0530

    Add files via upload

--- ml_app.py
+++ ml_app.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import json
 
 app = FastAPI()
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class model_input(BaseModel):
     pregnancies: int
